c45942dd10deb427c0e89ebe1c79899b0546e036
Braden Meyers <bjz-github@spwiz.com>
2026-01-05 10:20:54 -0700

    Apparently Chrony doesn't work to discipline the system clock inside docker.

--- docker/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Start GPSD in the background
-gpsd -N -n 'udp://0.0.0.0:3001' &
-
-# Start a bash shell to keep the container running and allow user interaction
-exec /bin/bash
